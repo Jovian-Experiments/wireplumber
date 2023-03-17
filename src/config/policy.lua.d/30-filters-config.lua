25a3918359082e1d2e7d8d0c179e79dcda2c7bed
Julian Bouzas <julian.bouzas@collabora.com>
2023-03-17 11:06:11 -0400

    modules: add new module-filters-api to configure audio filters easily from the policy.
    This module provides an API to link filter nodes using the logic configured in
    'policy.lua.d/30-filters-config.lua'. This configuration file allows grouping
    filters together (sorted by priority) to use a common target node. A node is
    considered a filter node if it has the node.link-group property.

---
+++ src/config/policy.lua.d/30-filters-config.lua
@@ -0,0 +1,70 @@
+-- uncomment to enable automatic filters policy logic
+--
+--[[
+
+-- The default filter metadata configuration when wireplumber starts. These
+-- values can change at runtime if the filters metadata changes.
+default_policy.filters_metadata = {
+  -- The sink filters configuration
+  ["sink-filters"] = {
+    {
+      ["stream-name"] = "output.virtual-sink",  -- loopback playback
+      ["node-name"] = "input.virtual-sink",     -- loopback sink
+      ["group"] = nil,  -- if nil, the default node will be used as target
+      ["enabled"] = true,
+      ["priority"] = 30,
+    },
+    {
+      ["stream-name"] = "filter-chain-playback",  -- filter-chain playback
+      ["node-name"] = "filter-chain-sink",        -- filter-chain sink
+      ["group"] = "speakers",  -- if nil, the default node will be used as target
+      ["enabled"] = true,
+      ["priority"] = 20,
+    },
+    {
+      ["stream-name"] = "echo-cancel-playback",  -- echo-cancel playback
+      ["node-name"] = "echo-cancel-sink",        -- echo-cancel sink
+      ["group"] = "speakers",  -- if nil, the default node will be used as target
+      ["enabled"] = false,
+      ["priority"] = 10,
+    },
+  },
+
+  -- The source filters configuration
+  ["source-filters"] = {
+    {
+      ["stream-name"] = "input.virtual-source",  -- loopback capture
+      ["node-name"] = "output.virtual-source",   -- loopback source
+      ["group"] = nil,  -- if nil, the default node will be used as target
+      ["enabled"] = true,
+      ["priority"] = 30,
+    },
+    {
+      ["stream-name"] = "filter-chain-capture",  -- filter-chain capture
+      ["node-name"] = "filter-chain-source",     -- filter-chain source
+      ["group"] = "microphone",  -- if nil, the default node will be used as target
+      ["enabled"] = false,
+      ["priority"] = 20,
+    },
+    {
+      ["stream-name"] = "echo-cancel-capture",  -- echo-cancel capture
+      ["node-name"] = "echo-cancel-source",     -- echo-cancel source
+      ["group"] = "microphone",  -- if nil, the default node will be used as target
+      ["enabled"] = false,
+      ["priority"] = 10,
+    }
+  },
+
+  -- The final target node properties of each group
+  ["groups-target"] = {
+    ["speakers"] = {
+      ["media.class"] = "Audio/Sink",
+      ["alsa.card_name"] = "my-speakers-card-name",
+    },
+    ["microphone"] = {
+      ["media.class"] = "Audio/Source",
+      ["alsa.card_name"] = "my-microphone-card-name"
+    }
+  }
+}
+--]]
